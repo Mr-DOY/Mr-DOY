126814f52068f650d3f5b4f802324dfa8476c74e
Mr-DOY <83167364+Mr-DOY@users.noreply.github.com>
2021-04-25 21:50:38 +0700

    Create bash spam.sh

---
+++ bash spam.sh
@@ -0,0 +1,11 @@
+Tools: 1.0 *_*
+        ______________________________
+              Author: Mr.Tr3v!0n
+            Team: Black Coder Crush
+            Tools: Spam Sms Halodoc
+           Telegram: @config_geratis
+        ______________________________
+         [26-04-21] | ©Copyright_2020
+
+        ╔════════════════════════╗
+        ║NO TARGET:
